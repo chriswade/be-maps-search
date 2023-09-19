48a86ea3739f3d5dbef5f4f2db5a3aa0aa6d4a93
Chris <hi@christopher.wade>
2023-09-20 07:32:03 +1000

    fixed missing async await

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ import { AppConfig, AutoCompleteAddressResponse } from './types';
  * @param address
  * @returns A Promise list containing the address responses
  */
-export const getAutoCompleteDetails = (address: string): Promise<AutoCompleteAddressResponse[]> => {
+export const getAutoCompleteDetails = async (address: string): Promise<AutoCompleteAddressResponse[]> => {
     // Typed config options to ensure type safety
     const config: AppConfig = {
         apiKey: process.env.TOMTOM_API_KEY as string,
     };
 
     // get autocomplete results
-    return getPlaceAutocomplete(config.apiKey, address);
+    return await getPlaceAutocomplete(config.apiKey, address);
 };
