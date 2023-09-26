1987babb3ad4190c929c5e257a6311bb749af1ce
Chris <hi@christopher.wade>
2023-09-27 09:57:12 +1000

    refactor

--- src/maps-api.ts
+++ src/maps-api.ts
@@ -1,20 +1,8 @@
 import axios from 'axios';
 import { AutoCompleteAddressResponse, CountryCode, TomTomAddressResult } from './types';
 
-// https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
-export async function getPlaceAutocomplete(key: string, address: string): Promise<AutoCompleteAddressResponse[]> {
-    if (!address) {
-        return Promise.reject(new Error('Address is required'));
-    }
-    const tomtomSearchApiEndpoint = `https://api.tomtom.com/search/2/search/${address}.json`;
-    const autocomplete = await axios.get(tomtomSearchApiEndpoint, {
-        params: {
-            key,
-            countrySet: CountryCode.AUS,
-            limit: 100,
-        },
-    });
-    return autocomplete.data.results.map((result: TomTomAddressResult): AutoCompleteAddressResponse => {
+export const mapTomTomResults = async (results: TomTomAddressResult[]): Promise<AutoCompleteAddressResponse[]> => {
+    return results.map((result: TomTomAddressResult): AutoCompleteAddressResponse => {
         const {
             id,
             address: { streetNumber, countryCode, country, freeformAddress, municipality },
@@ -28,4 +16,28 @@ export async function getPlaceAutocomplete(key: string, address: string): Promis
             municipality,
         };
     });
-}
+};
+
+// https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
+export const getPlaceAutocomplete = async (key: string, address: string): Promise<AutoCompleteAddressResponse[]> => {
+    if (!key) {
+        throw new Error('API key is required');
+    }
+    if (!address) {
+        throw new Error('Address is required');
+    }
+    const tomtomSearchApiEndpoint = `https://api.tomtom.com/search/2/search/${address}.json`;
+    try {
+        const autocomplete = await axios.get(tomtomSearchApiEndpoint, {
+            params: {
+                key,
+                countrySet: CountryCode.AUS,
+                limit: 100,
+            },
+        });
+
+        return mapTomTomResults(autocomplete.data.results);
+    } catch (error) {
+        throw new Error('failed to retrieve data');
+    }
+};
